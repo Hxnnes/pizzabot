ed64a159111f43580f7202b50e2e47d6dacc086a
Hxnnes <86929871+Hxnnes@users.noreply.github.com>
2021-07-05 15:09:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,44 +20,53 @@ async def on_message(message):
     if message.author == client.user:
       return
 
-    msg = message.content
-    text = msg.split()
+    text = message.content.split()
     user = 0
+
     if len(text) >= 2:
       user = text[1]
     
-    if msg.startswith('!addpizza'):
+    if text[0] == '!addpizza':
       update_user(user.lower())
       await message.channel.send(f"{user} hat schon {db[user.lower()]} mal Pizza gegessen. Penner!")
+      await message.delete()
 
-    if msg.startswith('!getpizza'):
+    if text[0] == '!getpizza':
       await message.channel.send(f"{user} hat schon {db[user.lower()]} mal Pizza gegessen. Penner!")
+      await message.delete()
 
-    if msg.startswith('!resetpizza'):
+    if text[0] == '!resetpizza':
       db[user.lower()] = 0
       await message.channel.send(f"{user}`s Pizzaspeicher wurde gelöscht")
+      await message.delete()
 
-    if msg.startswith('!setpizza'):
+    if text[0] == '!setpizza':
       db[user.lower()] = text[2]
       await message.channel.send(f"{user} hat schon {db[user.lower()]} mal Pizza gegessen. Penner!")
+      await message.delete()
 
-    if msg.startswith('!getallpizza'):
+    if text[0] == '!getallpizza':
       liste = ""
       for i in db.keys():
         liste += (str(i) + ": " + str(db[i]) + "\n")
       await message.channel.send(liste)
+      await message.delete()
 
-    if msg.startswith('!helppizza'):
+    if text[0] == '!helppizza':
       await message.channel.send("!addpizza user: erhöht Pizzazähler für user \n!getpizza user: gibt den Pizzazähler für user zurück \n!resetpizza user: setzt Pizzazähler für user zurück \n!setpizza user integer: setzt den Pizazähler von user auf den integer\n!getallpizza: gibt alle einträge aus\n Lustige neue Ideen sind gerne gesehen")
+      await message.delete()
 
-    if msg.startswith('!deletepizza') and str(message.author) =="Hxnnes#3925":
+    if text[0] == '!deletepizza' and message.author.id == int(os.environ['myID']):
       del db[user]
       await message.channel.send(f"{user} wurde gelöscht")
+      await message.delete()
 
-    if msg.startswith('!deleteallpizzayesimsure') and str(message.author) =="Hxnnes#3925":
+    if text[0] == '!deleteallpizzayesimsure' and message.author.id == int(os.environ['myID']):
       for i in db.keys():
         del db[i]
       await message.channel.send(f"keys: {''.join(db.keys())}")
+      await message.delete()
 
 keep_alive()
-client.run(os.getenv('TOKEN'))
+client.run(os.environ['TOKEN'])
+
